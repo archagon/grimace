d67e00abd2c6d1261e3d27c80b05bd7972b112bf
Alexei Baboulevitch <archagon@gmail.com>
2025-07-14 02:26:46 -0700

    Quit on close.

--- Grimace/AppDelegate.swift
+++ Grimace/AppDelegate.swift
@@ -21,4 +21,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return false
     }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 }
