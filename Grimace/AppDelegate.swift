5908eb0317a6060f75847d9d94be5b5edaa00cb1
Alexei Baboulevitch <archagon@gmail.com>
2025-07-14 03:46:34 -0700

    Adding icon. Escaping squircle jail.

--- Grimace/AppDelegate.swift
+++ Grimace/AppDelegate.swift
@@ -9,7 +9,14 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        if let icon = NSImage(named: "AppIcon") {
+            NSApplication.shared.dockTile.contentView = NSImageView(image: icon)
+            NSApplication.shared.dockTile.display()
+        }
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
